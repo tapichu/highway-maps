d971c9c59bb736a9a03038f4075c68417df997c7
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-08 17:13:16 -0600

    Added Carretera Model

---
+++ carreteras/admin.py
@@ -0,0 +1,5 @@
+from carreteras.models import Carretera
+from django.contrib import admin
+
+admin.site.register(Carretera)
+
--- carreteras/models.py
+++ carreteras/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Carretera(models.Model):
+    estado = models.CharField(max_length=50)
+    municipio = models.CharField(max_length=100)
+    localidad = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=200)
+    origen = models.CharField(max_length=100)
+    destino = models.CharField(max_length=100)
+    de_km = models.DecimalField(max_digits=10, decimal_places=4)
+    a_km = models.DecimalField(max_digits=10, decimal_places=4)
+    comentarios = models.CharField(max_length=1000)
+    version = models.IntegerField()
+
