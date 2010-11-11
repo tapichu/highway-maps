13c2801ef91b086b9fca5d88c10b5a520a6c033b
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-10 20:33:18 -0600

    Added display, search and filtering options to the admin console

--- carreteras/admin.py
+++ carreteras/admin.py
@@ -2,36 +2,47 @@ from carreteras.models import *
 from django.contrib import admin
 
 class EstadoAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['nombre']
 
 class MunicipioAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('nombre', 'estado')
+    list_filter = ['estado']
+    search_fields = ['nombre']
 
 class LocalidadAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('nombre', 'municipio')
+    search_fields = ['nombre']
 
 class RutaAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['numero']
 
 class CorredorAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['numero']
 
 class CarreteraAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['nombre', 'ruta']}),
         ('IDs', {'fields': ['identificador_nacional']}),
     ]
+    list_display = ('nombre', 'ruta', 'identificador_nacional')
+    list_filter = ['ruta']
+    search_fields = ['nombre', 'identificador_nacional']
 
 class TramoAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['nombre', 'carretera', 'corredor', 'origen', 'destino']}),
+        (None,
+            {'fields': ['nombre', 'carretera', 'corredor', 'origen', 'destino']}),
         (u'Caracter\u00edsticas',
             {'fields': ['tipo_red', 'km_inicio', 'km_fin', 'carriles', 'cuerpos']}),
+        (u'Ubicaci\u00f3n', {'fields': ['estados', 'municipios', 'localidad']}),
         (u'Geo Ubicaci\u00f3n', {
             'fields': ['latitud_inicio', 'longitud_inicio', 'latitud_fin', 'longitud_fin'],
             'classes': ['collapse']
         }),
     ]
+    list_display = ('nombre', 'carretera', 'corredor', 'origen', 'destino')
+    list_filter = ['carretera', 'corredor', 'tipo_red', 'carriles', 'cuerpos']
+    search_fields = ['nombre', 'carretera']
 
 class SubtramoAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -42,6 +53,9 @@ class SubtramoAdmin(admin.ModelAdmin):
             'classes': ['collapse']
         }),
     ]
+    list_display = ('nombre', 'tramo', 'km_inicio', 'km_fin')
+    list_filter = ['tramo']
+    search_fields = ['nombre', 'tramo']
 
 admin.site.register(Estado, EstadoAdmin)
 admin.site.register(Municipio, MunicipioAdmin)
