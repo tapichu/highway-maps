342adef75fd048d1327d9f876acda32469c150e5
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-14 23:32:45 -0600

    Added shortcuts to simplify views

--- carreteras/views.py
+++ carreteras/views.py
@@ -1,13 +1,14 @@
 from django.conf import settings
-from django.http import HttpResponse
-from django.template import Context, loader
+from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.template import RequestContext
 from carreteras.models import *
 
 def index(request):
-    t = loader.get_template('carreteras/index.html')
-    c = Context({
+    return render_to_response('carreteras/index.html', {
         'title': 'Buscador de carreteras',
         'MEDIA_URL': settings.MEDIA_URL,
     })
-    return HttpResponse(t.render(c))
 
