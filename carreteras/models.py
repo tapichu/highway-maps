4b520a699d9a7dc94e883e2ee8bc735c2065772f
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-16 09:40:12 -0600

    Customized the oneToMany relationship names

--- carreteras/models.py
+++ carreteras/models.py
@@ -18,14 +18,14 @@ class Estado(models.Model):
 
 class Municipio(models.Model):
     nombre = models.CharField(max_length=200)
-    estado = models.ForeignKey(Estado)
+    estado = models.ForeignKey(Estado, related_name='municipios')
 
     def __unicode__(self):
         return self.nombre
 
 class Localidad(models.Model):
     nombre = models.CharField(max_length=200)
-    municipio = models.ForeignKey(Municipio)
+    municipio = models.ForeignKey(Municipio, related_name='localidades')
 
     def __unicode__(self):
         return self.nombre
@@ -53,19 +53,19 @@ class Corredor(models.Model):
 class Carretera(models.Model):
     identificador_nacional = models.CharField(max_length=10)
     nombre = models.CharField(max_length=100)
-    ruta = models.ForeignKey(Ruta)
+    ruta = models.ForeignKey(Ruta, related_name='carreteras')
     fecha_modificacion = models.DateTimeField(u'Fecha \u00faltima modificaci\u00f3n', auto_now=True)
 
     def __unicode__(self):
         return self.nombre
 
 class Tramo(LineGeoLocation):
-    carretera = models.ForeignKey(Carretera)
+    carretera = models.ForeignKey(Carretera, related_name='tramos')
     nombre = models.CharField(max_length=100)
-    estados = models.ManyToManyField(Estado)
-    municipios = models.ManyToManyField(Municipio)
-    localidad = models.ForeignKey(Localidad)
-    corredor = models.ForeignKey(Corredor, blank=True, null=True)
+    estados = models.ManyToManyField(Estado, related_name='tramos')
+    municipios = models.ManyToManyField(Municipio, related_name='tramos')
+    localidad = models.ForeignKey(Localidad, related_name='tramos')
+    corredor = models.ForeignKey(Corredor, blank=True, null=True, related_name='tramos')
     tipo_red = models.CharField(max_length=1, choices=TIPO_RED_CHOICES)
     km_inicio = models.DecimalField(max_digits=10, decimal_places=4)
     km_fin = models.DecimalField(max_digits=10, decimal_places=4)
@@ -83,7 +83,7 @@ class Tramo(LineGeoLocation):
         return float(self.km_fin) - float(self.km_inicio);
 
 class Subtramo(LineGeoLocation):
-    tramo = models.ForeignKey(Tramo)
+    tramo = models.ForeignKey(Tramo, related_name='subtramos')
     nombre = models.CharField(max_length=100)
     km_inicio = models.DecimalField(max_digits=10, decimal_places=4)
     km_fin = models.DecimalField(max_digits=10, decimal_places=4)
