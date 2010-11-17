a7f77e11d14b25e7cf3de5b8ca69073acebad94b
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-17 02:22:22 -0600

    Calculate map center based on all tramos
    Draw markers for all tramos.
    
    Calculate route for the biggest tramo.

--- media/js/views/CarreteraDetailView.js
+++ media/js/views/CarreteraDetailView.js
@@ -11,11 +11,11 @@ define(['models/Tramo', './TramoView'], function(Tramo, TramoView) {
         events: {},
 
         initialize: function(params) {
-            _.bindAll(this, 'render', 'renderDetails');
-
+            _.bindAll(this, 'render', 'renderDetails', 'calculateRoute');
             this.model.bind('change', this.render);
 
             this.animationSpeed = params && params.animationSpeed || 500;
+            this.directionDisplay = new google.maps.DirectionsRenderer();
         },
 
         render: function() {
@@ -41,18 +41,23 @@ define(['models/Tramo', './TramoView'], function(Tramo, TramoView) {
             // Show section
             $this.slideDown(this.animationSpeed, function() {
                 // Render map
-                var tramo = that.model.get('tramos')[0];
-                var latlng = that.calculateMapCenter(tramo.latitud_inicio, tramo.longitud_inicio,
-                        tramo.latitud_fin, tramo.longitud_fin);
+                var tramos = that.model.get('tramos');
+                var latlng = that.calculateMapCenter(tramos);
                 var options = {
-                    zoom: 8,
+                    zoom: 7,
                     center: latlng,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
                 var map = new google.maps.Map(that.$('#map')[0], options);
-                var directionDisplay = new google.maps.DirectionsRenderer();
-                directionDisplay.setMap(map);
-                that.addTramoMarkers(map, directionDisplay, tramo);
+                that.directionDisplay.setMap(map);
+
+                _.each(tramos, function(tramo) {
+                    that.addTramoMarkers(map, tramo);
+                });
+
+                // Add directions for the biggest route
+                var tramo = _.max(tramos, function(t) { return t.longitud; });
+                that.calculateRoute(tramo);
             });
         },
 
@@ -62,15 +67,40 @@ define(['models/Tramo', './TramoView'], function(Tramo, TramoView) {
             this.$('#tramosTable').append(view.render(index).el);
         },
 
-        calculateMapCenter: function(latStart, lonStart, latEnd, lonEnd) {
-            var dLat = Math.abs(latStart - latEnd) / 2.0;
-            var latitude = latStart > latEnd ? latStart - dLat : latStart + dLat;
-            var dLon = Math.abs(lonStart - lonEnd) / 2.0;
-            var longitude = lonStart > lonEnd ? lonStart - dLon : lonEnd + dLon;
+        calculateMapCenter: function(tramos) {
+            var latStart = _.map(tramos, function(t) { return t.latitud_inicio; });
+            var latEnd = _.map(tramos, function(t) { return t.latitud_fin; });
+            var lonStart = _.map(tramos, function(t) { return t.longitud_inicio; });
+            var lonEnd = _.map(tramos, function(t) { return t.longitud_fin; });
+            var latitudes = latStart.concat(latEnd);
+            var longitudes = lonStart.concat(lonEnd);
+
+            var latMax = _.max(latitudes);
+            var latMin = _.min(latitudes);
+            var lonMax = _.max(longitudes);
+            var lonMin = _.min(longitudes);
+
+            var latitude = latMin + ((latMax - latMin) / 2); 
+            var longitude = lonMin + ((lonMax - lonMin) / 2);
             return new google.maps.LatLng(latitude, longitude);
         },
 
-        addTramoMarkers: function(map, directionDisplay, tramo) {
+        addTramoMarkers: function(map, tramo) {
+            var latlngStart = new google.maps.LatLng(tramo.latitud_inicio, tramo.longitud_inicio);
+            var latlngEnd = new google.maps.LatLng(tramo.latitud_fin, tramo.longitud_fin);
+
+            var start = new google.maps.Marker({
+                position: latlngStart,
+                map: map
+            });
+            var end = new google.maps.Marker({
+                position: latlngEnd,
+                map: map
+            });
+        },
+
+        calculateRoute: function(tramo) {
+            var that = this;
             var latlngStart = new google.maps.LatLng(tramo.latitud_inicio, tramo.longitud_inicio);
             var latlngEnd = new google.maps.LatLng(tramo.latitud_fin, tramo.longitud_fin);
 
@@ -81,17 +111,7 @@ define(['models/Tramo', './TramoView'], function(Tramo, TramoView) {
             };
             window.APP.directionsService.route(request, function(response, status) {
                 if (status === google.maps.DirectionsStatus.OK) {
-                    directionDisplay.setDirections(response);
-                } else {
-                    // Fallback to markers
-                    var start = new google.maps.Marker({
-                        position: latlngStart,
-                        map: map
-                    });
-                    var end = new google.maps.Marker({
-                        position: latlngEnd,
-                        map: map
-                    });
+                    that.directionDisplay.setDirections(response);
                 }
             });
         }
