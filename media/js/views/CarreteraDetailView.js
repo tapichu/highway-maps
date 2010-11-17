55ba827700c227caf184fb68f9b15721e9ae28c2
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2010-11-17 01:12:49 -0600

    Added google maps to carretera's detail view

--- media/js/views/CarreteraDetailView.js
+++ media/js/views/CarreteraDetailView.js
@@ -11,7 +11,7 @@ define(['models/Tramo', './TramoView'], function(Tramo, TramoView) {
         events: {},
 
         initialize: function(params) {
-            _.bindAll(this, 'render', 'renderTramos');
+            _.bindAll(this, 'render', 'renderDetails');
 
             this.model.bind('change', this.render);
 
@@ -24,26 +24,47 @@ define(['models/Tramo', './TramoView'], function(Tramo, TramoView) {
                 var that = this;
                 previousDetails.slideUp(this.animationSpeed, function() {
                     previousDetails.remove();
-                    that.renderTramos();
+                    that.renderDetails();
                 });
             } else {
-                this.renderTramos();
+                this.renderDetails();
             }
         },
 
-        renderTramos: function() {
+        renderDetails: function() {
+            var that = this;
             var $this = $(this.el)
                 .append(this.template(this.model.toJSON()))
                 .css({display: 'none'})
                 .appendTo($('#searchResults'));
             _.each(this.model.get('tramos'), this.addTramo, this);
-            $this.slideDown(this.animationSpeed);
+            // Show section
+            $this.slideDown(this.animationSpeed, function() {
+                // Render map
+                var tramo = that.model.get('tramos')[0];
+                var latlng = that.calculateMapCenter(tramo.latitud_inicio, tramo.longitud_inicio,
+                        tramo.latitud_fin, tramo.longitud_fin);
+                var options = {
+                    zoom: 8,
+                    center: latlng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                var map = new google.maps.Map(that.$('#map')[0], options);
+            });
         },
 
         addTramo: function(element, index) {
             var tramo = new Tramo(element);
             var view = new TramoView({model: tramo});
             this.$('#tramosTable').append(view.render(index).el);
+        },
+
+        calculateMapCenter: function(latStart, lonStart, latEnd, lonEnd) {
+            var dLat = Math.abs(latStart - latEnd) / 2.0;
+            var latitude = latStart > latEnd ? latStart - dLat : latStart + dLat;
+            var dLon = Math.abs(lonStart - lonEnd) / 2.0;
+            var longitude = lonStart > lonEnd ? lonStart - dLon : lonEnd + dLon;
+            return new google.maps.LatLng(latitude, longitude);
         }
 
     });
