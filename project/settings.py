b94acb8d7d3c804700fe994632ad4dd458b68506
Eduardo Lopez <eduardo.biagi@gmail.com>
2011-03-13 15:15:16 -0600

    Small fixes to settings.py
    Use development settings by default

--- project/settings.py
+++ project/settings.py
@@ -1,11 +1,7 @@
-# Django settings for poc_sct project.
 import os
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
-    # ('Eduardo Lopez', 'eduardo.lopez@jwmsolutions.com'),
+    # ('Eduardo Lopez', 'eduardo.biagi@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -46,7 +42,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.environ['PWD'] + '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -61,13 +57,6 @@ ADMIN_MEDIA_PREFIX = '/media/'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'bre7b$*6!iagzqyi1%q@%_ofbb)e!rawcnm9apx^%kf@b%)le!'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -76,11 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'poc_sct.urls'
-
-TEMPLATE_DIRS = (
-    os.environ['PWD'] + '/templates',
-)
+ROOT_URLCONF = 'project.urls'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -92,3 +77,15 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'carreteras',
 )
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), "templates"),
+)
+
